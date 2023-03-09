81d0665ae1dbe98c111a9b7ac15a8625542bef2b
DadaLV <vynohradova.l.a@gmail.com>
2023-03-09 03:33:49 +0200

    fix dropdown in navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,15 @@
 // import * as jquery from "jquery"
 // import "semantic-ui"
 // Loads all Semantic javascripts
-//= require semantic-ui
-//= require semantic-ui/modal
-//= require semantic-ui/dropdown
+// = require semantic-ui
+// = require semantic-ui/modal
+// = require semantic-ui/dropdown
+// import "@hotwired/turbo-rails"
+// import "controllers"
+ 
+// import "jquery"
+// import "semantic-ui"
+ 
+// $(document).on('turbo:load', function() {
+//   $('.ui.dropdown').dropdown();
+// })
