93e162a4fa12680e065c9b0c8166be1f374fd03f
DadaLV <vynohradova.l.a@gmail.com>
2023-03-11 23:54:07 +0200

    add destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
+
 end
